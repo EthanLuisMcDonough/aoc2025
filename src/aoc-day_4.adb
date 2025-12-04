00e9d241935a89f8b592a113298491cf821fef03
Ethan Luis McDonough <ethanluismcdonough@gmail.com>
2025-12-04 01:33:33 -0600

    Day 4 part 2

--- src/aoc-day_4.adb
+++ src/aoc-day_4.adb
@@ -1,77 +1,144 @@
-with Ada.Characters;
-with Ada.Characters.Latin_1;
 with Ada.Text_IO;
-with GNAT;
+with Ada.Containers.Vectors;
+with Ada.Characters.Latin_1;
 with GNAT.String_Split;
 
 use Ada;
 use GNAT;
 
 package body Aoc.Day_4 is
-   ROLL_CHAR : constant Character := '@';
+   package Int_Vec is new Containers.Vectors
+     (Index_Type => Natural, Element_Type => Integer);
 
-   procedure Part_One (Input : String) is
-      use GNAT.String_Split;
-
-      Lines : constant Slice_Set := Create (
-        From => Input, Mode => Multiple,
-        Separators => Characters.Latin_1.LF & "");
-      Accessible_Rolls : Natural := 0;
+   function Encode_Pos
+     (Rack : Roll_Rack; Row : Positive;
+      Col : Positive) return Natural
+   is
+      R : constant Natural := Natural'Val (Row) - 1;
+      C : constant Natural := Natural'Val (Col) - 1;
    begin
+      return R * Rack'Length (1) + C;
+   end Encode_Pos;
 
-      for I in 1 .. Slice_Count (Lines) loop
+   procedure Decode_Pos
+     (Rack : Roll_Rack; Pos : Natural;
+      Row : out Positive; Col : out Positive)
+   is
+      C : constant Natural := Pos mod Rack'Length (1);
+      R : constant Natural := Pos / Rack'Length (1);
+   begin
+      Row := Positive'Val (R + 1);
+      Col := Positive'Val (C + 1);
+   end Decode_Pos;
+
+   function Read_Rack (Input : String) return Roll_Rack is
+      Lines : constant String_Split.Slice_Set :=
+        String_Split.Create (
+          From => Input, Mode => String_Split.Multiple,
+          Separators => Characters.Latin_1.LF & "");
+   begin
+      declare
+         Rows : constant Positive := Positive'Val (
+           String_Split.Slice_Count (Lines));
+         Cols : constant Positive := Positive'Val (
+           String_Split.Slice (Lines, 1)'Length);
+         Rack : Roll_Rack (1 .. Rows, 1 .. Cols) :=
+           (others => (others => EMPTY));
+      begin
+         for I in 1 .. String_Split.Slice_Count (Lines) loop
+            declare
+               Row : constant String := String_Split.Slice (Lines, I);
+            begin
+               for J in Row'Range loop
+                  if Row (J) = '@' then
+                     Rack (Positive'Val (I),
+                       Positive'Val (J - Row'First + 1)) := ROLL;
+                  end if;
+               end loop;
+            end;
+         end loop;
+         return Rack;
+      end;
+   end Read_Rack;
+
+   function Can_Take_Roll
+     (Rack : Roll_Rack; Row : Positive;
+      Col : Positive) return Boolean
+   is
+      Adjacent : Natural := 0;
+      Lo_Check : constant Positive := Positive'Val (
+        Natural'Max (Natural'Val (Col) - 1, 1));
+      Hi_Check : constant Integer := Positive'Min (
+        Col + 1, Rack'Last (2));
+   begin
+      if Rack (Row, Col) /= ROLL then
+         return False;
+      end if;
+
+      Adjacent := Boolean'Pos (
+        Col > 1 and then Rack (Row, Col - 1) = ROLL) +
+        Boolean'Pos (Col < Rack'Length (1) and then
+          Rack (Row, Col + 1) = ROLL);
+
+      if Row > 1 then
+         for K in Lo_Check .. Hi_Check loop
+            if Rack (Row - 1, K) = ROLL then
+               Adjacent := Adjacent + 1;
+            end if;
+         end loop;
+      end if;
+
+      if Row < Rack'Last (1) then
+         for K in Lo_Check .. Hi_Check loop
+            if Rack (Row + 1, K) = ROLL then
+               Adjacent := Adjacent + 1;
+            end if;
+         end loop;
+      end if;
+
+      return Adjacent < 4;
+   end Can_Take_Roll;
+
+   function Take_Rolls (Rack : in out Roll_Rack) return Natural is
+      To_Take : Int_Vec.Vector;
+   begin
+      for I in Rack'Range (1) loop
+         for J in Rack'Range (2) loop
+            if Can_Take_Roll (Rack, I, J) then
+               Int_Vec.Append (To_Take, Encode_Pos (Rack, I, J));
+            end if;
+         end loop;
+      end loop;
+
+      for Pos of To_Take loop
          declare
-            Row : constant String := Slice (Lines, I);
+            Row, Col : Positive;
          begin
-            for J in Row'Range loop
-               if Row (J) = ROLL_CHAR then
-                  declare
-                     Adjacent : Natural := Boolean'Pos (
-                       J > Row'First and then Row (J - 1) = ROLL_CHAR) +
-                       Boolean'Pos (J < Row'Last and then
-                         Row (J + 1) = ROLL_CHAR);
-                     J_Norm : constant Integer := J - Row'First + 1;
-                     Lo_Check : constant Integer :=
-                       Integer'Max (J_Norm - 1, 1);
-                     Hi_Check : constant Integer :=
-                       Integer'Min (J_Norm + 1, Row'Length);
-                  begin
-                     if I > 1 then
-                        declare
-                           Prev : constant String := Slice (Lines, I - 1);
-                        begin
-                           for K in Lo_Check .. Hi_Check loop
-                              Adjacent := Adjacent + Boolean'Pos (
-                                Prev (Prev'First + K - 1) = ROLL_CHAR);
-                           end loop;
-                        end;
-                     end if;
-
-                     if I /= Slice_Count (Lines) then
-                        declare
-                           Next : constant String := Slice (Lines, I + 1);
-                        begin
-                           for K in Lo_Check .. Hi_Check loop
-                              Adjacent := Adjacent + Boolean'Pos (
-                                Next (Next'First + K - 1) = ROLL_CHAR);
-                           end loop;
-                        end;
-                     end if;
-
-                     if Adjacent < 4 then
-                        Accessible_Rolls := Accessible_Rolls + 1;
-                     end if;
-                  end;
-               end if;
-            end loop;
+            Decode_Pos (Rack, Pos, Row, Col);
+            Rack (Row, Col) := EMPTY;
          end;
       end loop;
 
-      Ada.Text_IO.Put_Line ("Accessible rolls: " & Accessible_Rolls'Image);
+      return Integer'Val (Int_Vec.Length (To_Take));
+   end Take_Rolls;
+
+   procedure Part_One (Input : String) is
+      Rack : Roll_Rack := Read_Rack (Input);
+      Accessible_Rolls : constant Integer := Take_Rolls (Rack);
+   begin
+      Text_IO.Put_Line ("Accessible rolls: " & Accessible_Rolls'Image);
    end Part_One;
 
    procedure Part_Two (Input : String) is
+      Rack : Roll_Rack := Read_Rack (Input);
+      Total_Removed : Integer := 0;
+      Local_Removed : Integer := 0;
    begin
-      null;
+      loop
+         Local_Removed := Take_Rolls (Rack);
+         exit when Local_Removed = 0;
+         Total_Removed := Total_Removed + Local_Removed;
+      end loop;
+      Text_IO.Put_Line ("Total removed: " & Total_Removed'Image);
    end Part_Two;
 end Aoc.Day_4;
--- src/aoc-day_4.ads
+++ src/aoc-day_4.ads
@@ -1,4 +1,23 @@
 package Aoc.Day_4 is
+   type Rack_Slot is (EMPTY, ROLL);
+   type Roll_Rack is array (Positive range <>,
+     Positive range <>) of Rack_Slot;
+
+   function Read_Rack (Input : String) return Roll_Rack;
+
+   function Encode_Pos
+     (Rack : Roll_Rack; Row : Positive;
+      Col : Positive) return Natural;
+   procedure Decode_Pos
+     (Rack : Roll_Rack; Pos : Natural;
+      Row : out Positive; Col : out Positive);
+
+   function Can_Take_Roll
+     (Rack : Roll_Rack; Row : Positive;
+      Col : Positive) return Boolean;
+   function Take_Rolls (Rack : in out Roll_Rack)
+     return Natural;
+
    procedure Part_One (Input : String);
    procedure Part_Two (Input : String);
 end Aoc.Day_4;
