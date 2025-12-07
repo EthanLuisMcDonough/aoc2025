1607002970d9d9cf587ac71df2eebdcfbc889f17
Ethan Luis McDonough <ethanluismcdonough@gmail.com>
2025-12-07 01:41:08 -0600

    Part 7 day 2

--- src/aoc-day_7.adb
+++ src/aoc-day_7.adb
@@ -1,16 +1,28 @@
+with Interfaces;
+with Ada.Strings.Fixed;
 with Ada.Text_IO;
 with GNAT.String_Split;
 with Aoc.Common;
 
 use Ada;
+use Interfaces;
 
 package body Aoc.Day_7 is
-   procedure Part_One (Input : String) is
+   function Traverse (Input : String) return Traversal_Data is
       use GNAT.String_Split;
+
       Lines : constant Slice_Set := Common.Split_Lines (Input);
       Prev : String := Slice (Lines, 1);
+
+      type Possible_Paths is array (1 .. Prev'Length)
+        of Unsigned_64;
+
+      Counts : Possible_Paths := (others => 0);
+      Prev_Counts : Possible_Paths := (others => 0);
       Splits : Natural := 0;
+      Total_Paths : Unsigned_64 := 0;
    begin
+      Prev_Counts (Strings.Fixed.Index (Prev, "S")) := 1;
       for I in 2 .. Slice_Count (Lines) loop
          declare
             Line : String := Slice (Lines, I);
@@ -24,11 +36,14 @@ package body Aoc.Day_7 is
                begin
                   if Above in 'S' | '|' then
                      case Current is
-                        when '.' =>
+                        when '.' | '|' =>
                            Line (C_Ind) := '|';
+                           Counts (J) := Counts (J) + Prev_Counts (J);
                         when '^' =>
                            Line (C_Ind + 1) := '|';
                            Line (C_Ind - 1) := '|';
+                           Counts (J - 1) := Counts (J - 1) + Prev_Counts (J);
+                           Counts (J + 1) := Counts (J + 1) + Prev_Counts (J);
                            Splits := Splits + 1;
                         when others =>
                            null;
@@ -36,14 +51,29 @@ package body Aoc.Day_7 is
                   end if;
                end;
             end loop;
+
             Prev := Line;
+            Prev_Counts := Counts;
+            Counts := (others => 0);
          end;
       end loop;
-      Text_IO.Put_Line ("Splits: " & Splits'Image);
+
+      for I of Prev_Counts loop
+         Total_Paths := Total_Paths + I;
+      end loop;
+
+      return (Splits => Splits, Paths => Total_Paths);
+   end Traverse;
+
+   procedure Part_One (Input : String) is
+      Data : constant Traversal_Data := Traverse (Input);
+   begin
+      Text_IO.Put_Line ("Splits: " & Data.Splits'Image);
    end Part_One;
 
    procedure Part_Two (Input : String) is
+      Data : constant Traversal_Data := Traverse (Input);
    begin
-      null;
+      Text_IO.Put_Line ("Paths: " & Data.Paths'Image);
    end Part_Two;
 end Aoc.Day_7;
--- src/aoc-day_7.ads
+++ src/aoc-day_7.ads
@@ -1,4 +1,14 @@
+with Interfaces;
+
 package Aoc.Day_7 is
+   type Traversal_Data is record
+      Splits : Natural;
+      Paths : Interfaces.Unsigned_64;
+   end record;
+
+   function Traverse (Input : String)
+     return Traversal_Data;
+
    procedure Part_One (Input : String);
    procedure Part_Two (Input : String);
 end Aoc.Day_7;
